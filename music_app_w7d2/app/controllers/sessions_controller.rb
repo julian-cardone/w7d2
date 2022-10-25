5029c65a9820a87eac32120c8c6d2df548d4979a
Julian Cardone <juliancardone1@gmail.com>
2022-10-25 14:43:10 -0400

    almost finished part 1

--- music_app_w7d2/app/controllers/sessions_controller.rb
+++ music_app_w7d2/app/controllers/sessions_controller.rb
@@ -1,3 +1,28 @@
 class SessionsController < ApplicationController
-    
+
+    before_action :require_logged_in, only: [:new, :create]
+    before action :require_logged_out, only: [:destroy] 
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], [:user][:password])
+
+        if @user
+            login!(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = ['invalid credentials']
+            render :new
+        end
+    end
+
+    def destroy
+        logout!
+        flash[:messages] = ["Successfully logged out."]
+        redirect_to new_session_url
+    end
+
 end
